9478e5d2368200b18b53efc245417d47e73c4bbc
KingAJ19 <anelesokwane@gmail.com>
2024-07-02 12:38:51 +0200

    stat

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ var myArray = [
         'name': 'Lyndon', 'goals': '1', 'assists': '1', 'position': 'DEF'
     },
     {
-        'name': 'Leighton', 'goals': '1', 'assists': '0', 'position': 'LW'
+        'name': 'Leighton', 'goals': '2', 'assists': '0', 'position': 'LW'
     },
     {
         'name': 'Adrian', 'goals': '0', 'assists': '0', 'position': 'LW'
